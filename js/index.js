537feb07ffbe64ce6c74cfe6d810ca0dce2446ff
Michael Lobman <michael.lobman@gmail.com>
2021-11-15 12:23:37 -0500

    commit tree 3c95e90dd768e5296c296ac7957d949c2e1674e5

--- js/index.js
+++ js/index.js
@@ -1,1 +1,71 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", fetchBooks);
+const myProfile = {
+    id: 99,
+    username: "mlobman"
+};
+
+
+function fetchBooks(){
+    fetch("http://localhost:3000/books")
+    .then(resp => resp.json())
+    .then(books => {
+        const bookList = document.getElementById('list');
+        const showPanel = document.getElementById('show-panel');
+        for(const book of books) {
+            const bookTitle = book["title"];
+            const bookThumbnail = book["img_url"];
+            const bookDescription = book["description"];
+            const bookEntry = document.createElement('li');
+            const bookUsers = book["users"];
+            const bookId = book["id"]
+            bookEntry.textContent = bookTitle;
+            bookList.append(bookEntry);
+            bookEntry.addEventListener('click', function updatePanel() {
+                showPanel.innerHTML = `
+                <img src=${bookThumbnail}</img>
+                <h3>${bookTitle}</h3>
+                <p>${bookDescription}/<p>
+                <ul id="user-list">
+                </ul>
+                <button id="like-button">LIKE</button>
+                ` 
+                for(const user of bookUsers){
+                    const userEntry = document.createElement('li');
+                    userEntry.textContent = user["username"];
+                    document.getElementById('user-list').append(userEntry)
+                }    
+                const likeButton = document.getElementById('like-button');
+                likeButton.addEventListener('click', function(){
+                    if (bookUsers.find(element => element === myProfile)) {
+                        bookUsers.pop();
+                        fetch(`http://localhost:3000/books/${bookId}`, {
+                            method: "PATCH",
+                            headers: {
+                                "Content-Type": "application/json",
+                                Accept: "application/json",
+                            },
+                            body: JSON.stringify({
+                                "users": bookUsers,
+                            })
+                        })
+                    } else {
+                        bookUsers.push(myProfile);
+                        fetch(`http://localhost:3000/books/${bookId}`, {
+                            method: "PATCH",
+                            headers: {
+                                "Content-Type": "application/json",
+                                Accept: "application/json",
+                            },
+                            body: JSON.stringify({
+                                "users": bookUsers,
+                            })
+                        })
+                    }
+                    updatePanel();
+                })    
+            })
+
+        }
+    })
+}
+
